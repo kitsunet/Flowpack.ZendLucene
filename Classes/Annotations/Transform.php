c08d473cedcd29b4d9a458564e4d8a9c978d2817
Christian Müller <christian@kitsunet.de>
2014-02-11 22:35:03 +0100

    [TASK] Intermediate working state.

---
+++ Classes/Annotations/Transform.php
@@ -0,0 +1,32 @@
+<?php
+namespace Flowpack\ZendLucene\Annotations;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Flowpack.ElasticSearch".*
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use Doctrine\Common\Annotations\Annotation as DoctrineAnnotation;
+
+/**
+ * @Annotation
+ * @DoctrineAnnotation\Target("PROPERTY")
+ */
+final class Transform {
+
+	/**
+	 * @var string
+	 */
+	public $type;
+
+	/**
+	 * @var array
+	 */
+	public $options;
+}
+
